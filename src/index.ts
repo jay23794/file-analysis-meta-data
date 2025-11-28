40d2b3b9510124a01e912253840b168f298d9b48
jay parmar <jayprmr27@gmail.com>
2025-11-28 10:34:11 +0530

    inti

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
